44c313d79ee6736df736dba0b5d0e3c9d359cb7e
ruchi672 <61773688+ruchi672@users.noreply.github.com>
2023-08-18 16:50:29 +0530

    Update AppController.java

--- src/main/java/com/charan/homepageappspringbootjar/controller/AppController.java
+++ src/main/java/com/charan/homepageappspringbootjar/controller/AppController.java
@@ -10,6 +10,11 @@ public class AppController {
 	public String loadHomepage() {
 		return "index";
 	}
+
+	@RequestMapping("/admin")
+   	public String adminPage() {
+        	return "admin";
+   	 }
 	
 
 	
